d49b98d3ee434d101ae421b7e92b174780cb96f4
Queengirl18 <bouhilaoumaima@gmail.com>
2024-04-19 14:42:18 -0700

    models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"Module for base class"
+class Base:
+"A representation of the base of our oop hierarchy"
+__nb_objects = 0
+def __init__(self, id=None):
+    "constructor"
+    ifid is not None:
+        self.id = id
+    else
+    base.__nb_object +- 1
+    self.id = Base.__nb_object
+    
